258ba45d757959480e5c7f86000346c73a9f3496
secureweb16 <secureweb16@gmail.com>
2025-02-06 18:01:38 +0530

    06-02-2025

--- controller/bookingController.js
+++ controller/bookingController.js
@@ -1,6 +1,8 @@
 const { default: mongoose } = require('mongoose');
 const USER = require('../model/userModel');
+const BOOKING = require('../model/bookingModel');
 const stripe = require('stripe')('sk_test_51NidCYLrsyYxaIR1iN2HAxIKTZvcDWqAYK7DOyamLw091qIlEcYoizpSEfpYQiZcAhfdooawDbJGzxK2e5vJxcz300XjWWIfyE');
+
 const createPaymentIntent = async (req,res)=>{
   try {
     const student = await USER.findOne({_id: new mongoose.Types.ObjectId(req.body.userId)},{ name:0,email:0,image:0,password:0,role:0,status:0,createdAt:0,updatedAt:0 });
@@ -17,6 +19,19 @@ const createPaymentIntent = async (req,res)=>{
   }
 }
 const createBookingDetails = async (req,res)=>{
-  console.log(req.body);
+  try {
+    const bookingRes = new BOOKING(req.body);
+    const bookres = await bookingRes.save();
+    res.json({
+      message:"Booking saved sucessfully",
+      status:true,
+      send:(bookres)
+    })
+  } catch (error) {
+    res.json({
+      message:"Booking not saved",
+      status:false
+    })
+  }
 }
 module.exports = {createPaymentIntent,createBookingDetails};
